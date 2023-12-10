02066b78ed759be5f266a34ac7586f8c8d10555e
Paul Stern <paulstern45@gmail.com>
2023-12-10 10:57:31 +0300

    Try to solve the problem

--- 0004/0004.go
+++ 0004/0004.go
@@ -4,12 +4,27 @@ package main
 
 import "fmt"
 
+const t = 91 * 99
+
 func main() {
-	fmt.Println(91 * 9999)
+	fmt.Println()
+	fmt.Println(isPalindrome(t))
+	// fmt.Println(isPalindrome(90 * 99))
+	// fmt.Printf("%.*s", 2, fmt.Sprint(t))
+	// fmt.Printf("%.*s", 2, fmt.Sprint(t))
+	s := fmt.Sprint(t)
+	fmt.Println(s, s[0])
 }
 
 func isPalindrome(n int) bool {
-	b := fmt.Sprintf("%d", n)
-	e := fmt.Sprintf("%d", n)
-	return b == e
+	b := fmt.Sprint(n / 100)
+	e := fmt.Sprintf("%02d", n%100)
+	// fmt.Printf("%s: %s", b, e)
+	fmt.Println(n, b, e)
+	for i := 0; i < 3; i++ {
+		if b[i] != e[len(e)-1] {
+			return false
+		}
+	}
+	return true
 }
